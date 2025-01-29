69f21652965b406dfba32106bd177b58f4a81cae
Aditya Pratap Singh <thisisaditya.me@gmail.com>
2025-01-29 22:43:31 +0530

    optimized bubble sort

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main() {
     std::mt19937 gen(rd()); // Seed the generator
     std::uniform_int_distribution<> distr(0, 1000000); // Define the range
 
-    long long int n = 10000;
+    long long int n = 1000000;
 
     std::vector<long long int> unique_numbers(n);
     for (long long int i = 0; i < n; i++) {
@@ -17,13 +17,29 @@ int main() {
 
     auto start = std::chrono::high_resolution_clock::now(); // Start time
 
-    // bubble sort
+    // // bubble sort
+    // for (long long int i = 0;i < n; i++) {
+    //     for (long long int j = 0;j < n - 1; j++) {
+    //         if (unique_numbers[j] > unique_numbers[j + 1]) {
+    //             long long int temp = unique_numbers[j];
+    //             unique_numbers[j] = unique_numbers[j + 1];
+    //             unique_numbers[j + 1] = temp;
+    //         }
+    //     }
+    // }
+
+    // optimized bubble sort
     for (long long int i = 0;i < n; i++) {
+        bool isSwapped = false;
         for (long long int j = 0;j < n - 1; j++) {
             if (unique_numbers[j] > unique_numbers[j + 1]) {
                 long long int temp = unique_numbers[j];
                 unique_numbers[j] = unique_numbers[j + 1];
                 unique_numbers[j + 1] = temp;
+                isSwapped = true;
+            }
+            if (!isSwapped) {
+                break;
             }
         }
     }
