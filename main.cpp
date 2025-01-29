44f590390bfb57c630d4d5f77e357d9b654ac068
Aditya Pratap Singh <thisisaditya.me@gmail.com>
2025-01-29 22:56:21 +0530

    quick sort

--- main.cpp
+++ main.cpp
@@ -2,13 +2,36 @@
 #include <iostream>
 #include <chrono>
 #include <random>
+#include <bits/stdc++.h>
+using namespace std;
+
+int partition(vector<long long int>& vec, int low, int high) {
+    int pivot = vec[high];
+    int i = (low - 1);
+    for (int j = low; j <= high - 1; j++) {
+        if (vec[j] <= pivot) {
+            i++;
+            swap(vec[i], vec[j]);
+        }
+    }
+    swap(vec[i + 1], vec[high]);
+    return (i + 1);
+}
+
+void quickSort(vector< long long int>& vec, int low, int high) {
+    if (low < high) {
+        int pi = partition(vec, low, high);
+        quickSort(vec, low, pi - 1);
+        quickSort(vec, pi + 1, high);
+    }
+}
 
 int main() {
     std::random_device rd; // Obtain a random number from hardware
     std::mt19937 gen(rd()); // Seed the generator
     std::uniform_int_distribution<> distr(0, 1000000); // Define the range
 
-    long long int n = 100000;
+    long long int n = 10000;
 
     std::vector<long long int> unique_numbers(n);
     for (long long int i = 0; i < n; i++) {
@@ -57,16 +80,19 @@ int main() {
     //     unique_numbers[minIndex] = temp;
     // }
 
-    // insertion sort
-    for (int i = 1;i < n; i++) {
-        int j = i;
-        while (j > 0 && unique_numbers[j] < unique_numbers[j - 1]) {
-            int temp = unique_numbers[j];
-            unique_numbers[j] = unique_numbers[j - 1];
-            unique_numbers[j - 1] = temp;
-            j--;
-        }
-    }
+    // // insertion sort
+    // for (int i = 1;i < n; i++) {
+    //     int j = i;
+    //     while (j > 0 && unique_numbers[j] < unique_numbers[j - 1]) {
+    //         int temp = unique_numbers[j];
+    //         unique_numbers[j] = unique_numbers[j - 1];
+    //         unique_numbers[j - 1] = temp;
+    //         j--;
+    //     }
+    // }
+
+    // quick sort
+    quickSort(unique_numbers, 0, n - 1);
 
     auto end = std::chrono::high_resolution_clock::now(); // End time
     std::chrono::duration<double> duration = end - start; // Calculate duration
